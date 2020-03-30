9434666dc2225966efd958a81cf992fe345f8b2b
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-03-30 15:24:01 -0600

    properly arranged file directories according to deep-dive style guide

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PWP Milestone 1</title>
+
+</head>
+<body>
+<div>
+	<h1>PURPOSE, AUDIENCE & GOAL</h1>
+	<p><strong>Purpose:</strong> This project focus is to create a simple front end web page. credibility for my rental arbitrage
+		Display my portfolio to show track records</p>
+
+	<p><strong>Audience:</strong>The intent of the site is for a professional audience and is targeted towards<br>
+		residential property owners such as apartments, condos and home owners.</p>
+	<p><strong>GOAL:</strong> The primary goal of this website is to show credibility for my rental arbitrage to residential property owner and managers.<br> As well as to educate about my client's business.
+			<br>
+			To book short term rentals for guest of all types like travel nurses, business trips, tourist and<br>
+			home insurance clients.</p>
+
+	<h2>Persona</h2>
+
+	<p><strong>Name:</strong> Maya Moore</p>
+
+	<p><strong>Age:</strong> 30</p>
+
+	<p><strong>Profession:</strong> Manager at Lincoln Property company Apartment Property Management. They are extending
+		their franchise to<br> New Mexico and have 5 apartment buildings in the city. They are in search of tenants.</p>
+
+	<p><strong>Technology:</strong> Windows 10 Pro 64-bit user. Uses a 9th Generation Intel Core i5 processor HP ProDesk 600 G5 Microtower PC at work,<br>
+		a Dell G7 17 Gaming Laptop at home. And an iphone X with 4G unlimited data.</p>
+
+	<p><strong>Attitudes and Behaviors:</strong> Open minded to invest in several businesses, loves to read, pays attention to details.<br>
+		Plays basketball for workout during the weekends, surfs the web for innvesting in real estate while cooling down after workouts.<br> Maya is a huge facebook user,
+		and is in multiple real estate groups.</p>
+
+	<p><strong>Frustrations and Needs:</strong> Maya wants to find ways to get more tenants for
+		Lincoln Property Company in New Mexico,<br>
+		and is looking for other legal ways to acquire them. Maya is not aware of other ways to get
+		her apartments filled up. So she is looking for<br>
+		information online. All she needs is a website that would draw her attention towards it
+		and simplify their business in lame man's term before navigating it thoroughly.
+	</p>
+
+	<p><strong>Goals:</strong> Maya's goal is to find other ways to get her apartments leased to residents.
+	</p>
+
+	<p><strong>User Story:</strong> As a user, I need to get a brief info about this business, and see a portfolio with links to the other business partners with testimonies.
+	</p>
+
+</div>
+</body>
+
+
+</html>
