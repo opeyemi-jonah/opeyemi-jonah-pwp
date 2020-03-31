b5f7fd78fe8aa0a8e0afb7ce479d6c97f75e8cf1
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-03-31 00:46:19 -0600

    milestone-2.php created

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PWP Milestone 2</title>
+
+</head>
+<body>
+<h2>Content Strategy</h2>
+<p>This site will consist of a single web page with 3 buttons to quickly navigate to a section of the page.
+The buttons will contain the following:</p>
+
+<p><strong>Welcome:</strong> This will be the company logo and the name of the company with a wallpaper beneath.</p>
+
+<p><strong>About Me:</strong> </p>
+
+</body>
+</html>
