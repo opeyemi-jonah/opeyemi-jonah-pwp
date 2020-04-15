c20e887c0d1c2490c5ff65e3c3590388da2f2f74
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-04-14 21:28:08 -0600

    re made milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,15 +8,13 @@
 <body>
 <div>
 	<h1>PURPOSE, AUDIENCE & GOAL</h1>
-	<p><strong>Purpose:</strong> This project focus is to create a simple front end web page. credibility for my rental arbitrage
-		Display my portfolio to show track records</p>
+	<p><strong>Purpose:</strong> This project focus is to create a simple front end web page showcasing my skills<br>
+	 and a short story about me.</p>
 
-	<p><strong>Audience:</strong>The intent of the site is for a professional audience and is targeted towards<br>
-		residential property owners such as apartments, condos and home owners.</p>
-	<p><strong>GOAL:</strong> The primary goal of this website is to show credibility for my rental arbitrage to residential property owner and managers.<br> As well as to educate about my client's business.
-			<br>
-			To book short term rentals for guest of all types like travel nurses, business trips, tourist and<br>
-			home insurance clients.</p>
+	<p><strong>Audience:</strong>The intent of this website for any company that needs a web solution or software,<br>
+	 from small businesses to corporate. As well as independent software developers that need a <br>collaboration on  their projects. </p>
+	<p><strong>GOAL:</strong>To acquire a full or a part-time employment as web developer or software developer.
+	</p>
 
 	<h2>Persona</h2>
 	<img src="maya.jpg" alt="photo of maya moore">
@@ -24,29 +22,30 @@
 
 	<p><strong>Age:</strong> 30</p>
 
-	<p><strong>Profession:</strong> Manager at Lincoln Property company Apartment Property Management. They are extending
-		their franchise to<br> New Mexico and have 5 apartment buildings in the city. They are in search of tenants.</p>
+	<p><strong>Profession:</strong> Recruiter for Mavensoft Technologies. Mavensoft Technologies are short of staff<br>
+	 and are looking for fresh programmers to keep up with the current trend in the software market. <br>To groom as well
+	to become loyal employees in the nearest future.</p>
 
-	<p><strong>Technology:</strong> Windows 10 Pro 64-bit user. Uses a 9th Generation Intel Core i5 processor HP ProDesk 600 G5 Microtower PC at work,<br>
+	<p><strong>Technology:</strong> Windows 10 Pro 64-bit user. Uses a 9th Generation Intel Core i5 processor <br>
+		HP ProDesk 600 G5 Microtower PC at work,<br>
 		a Dell G7 17 Gaming Laptop at home. And an iphone X with 4G unlimited data.</p>
 
-	<p><strong>Attitudes and Behaviors:</strong> Open minded to invest in several businesses, loves to read, pays attention to details.<br>
-		Plays basketball for workout during the weekends, surfs the web for innvesting in real estate while cooling down after workouts.<br> Maya is a huge facebook user,
-		and is in multiple real estate groups.</p>
+	<p><strong>Attitudes and Behaviors:</strong> Reads in a fast manner by looking for keywords when checking a portfolio.
+	<br>Maya spends more time on social media like instagram, facebook, meetup, and linkedIn.
+	</p>
 
-	<p><strong>Frustrations and Needs:</strong> Maya wants to find ways to get more tenants for
-		Lincoln Property Company in New Mexico,<br>
-		and is looking for other legal ways to acquire them. Maya is not aware of other ways to get
-		her apartments filled up. So she is looking for<br>
-		information online. All she needs is a website that would draw her attention towards it
-		and simplify their business in lame man's term before navigating it thoroughly.
+	<p><strong>Frustrations and Needs:</strong>
+		Maya needs to hire an intern that's a full-stack web developer with little<br> to no experience but with a degree or
+		 certificate showing proof of knowledge in web development. <br>But Maya is having a hard time selecting candidates
+		in this competitive market. She wants to<br> make sure she hires the right combination of skills and personality
+		 for the company who will be<br> willing to take a lower salary for at least 2 years.
 	</p>
 
 	<p><strong>Goals:</strong> Maya's goal is to find other ways to get her apartments leased to residents.
 	</p>
 
-	<p><strong>User Story:</strong> As a user, I need to get a brief info about this business, and see a portfolio with links to the other business partners with testimonies.
-	</p>
+	<p><strong>User Story:</strong> As a user, I need to determine a candidate at first glance for an internship level<br> full stack web
+		developer position.  </p>
 
 </div>
 </body>
