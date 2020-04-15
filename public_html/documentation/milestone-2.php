76db29d899f8b12ba4e42eb27563c681d2d0a79a
Opeyemi Jonah <gavrieljonah@gmail.com>
2020-04-14 23:36:50 -0600

    re made milestone-2.php

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -10,26 +10,24 @@
 <p>This site will consist of a single web page with 3 buttons to quickly navigate to a section of the page.
 The buttons will contain the following:</p>
 
-<p><strong>Welcome:</strong> This will be the company logo and the name of the company with a wallpaper beneath.</p>
+<p><strong>Welcome:</strong> This will be my logo and my name on a wallpaper.</p>
 
-<p><strong>About Me:</strong> In this section I will briefly explain my business strategy with a proposal.</p>
+<p><strong>About Me:</strong> In this section I will have a short history about me.</p>
 
-<p><strong>Portfolio:</strong> In this section I want to show other properties
- have done business with. There will be some images of the <br>homes I have leased in the past and currently, this images will have
-links to the property manager's websites. </p>
+<p><strong>Portfolio:</strong> In this section I will list my skills and show some of my projects. </p>
 
 <p><strong>Contact:</strong> This will be a simple contact form with my business email, phone number and facebook page.</p>
 
 
 <h3>Full Screen Layout</h3>
-<img src="desktopWireframe.png" alt="fullscreen layout display image">
+<img src="new-wireframe1.png" alt="fullscreen layout display image">
 
 <br>
 <br>
 <p></p>
 
 <h3>Mobile Layout</h3>
-<img src="mobileWireframe.png" alt="mobile layout display image">
+<img src="new-wireframe2.png" alt="mobile layout display image">
 
 </body>
 </html>
